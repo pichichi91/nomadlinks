4ff6f42111a8211d4ebbb2d59c23ef5c6f8d5714
phn <pichichi91@users.noreply.github.com>
2021-09-12 00:32:34 +0200

    Fix Spaces

--- pages/index.js
+++ pages/index.js
@@ -13,16 +13,16 @@ export default function Home({ links, hasMore }) {
         <title>NomadLinks</title>
       </Head>
 
-      <header className="container mx-auto mt-20 p-4">
-        <h1 className="drop-shadow text-center text-8xl font-black font-lobster text-green-700">NomadLinks</h1>
+      <header className="container  mx-auto mt-32 p-4">
+        <h1 className="drop-shadow text-center  text-6xl md:text-8xl font-black font-lobster text-green-700">NomadLinks</h1>
 
       </header>
 
       <main >
 
-        <div className="container mb-16 mx-auto mt-12 p-4 rounded-3xl border-2 border-green-100	 bg-white shadow-2xl">
+        <div className="container w-5/6 md:w-full mb-16 mx-auto mt-20 p-4 rounded-3xl border-2 border-green-100	 bg-white shadow-2xl">
 
-          <div className="ml-6 mt-8 flex flex-wrap">
+          <div className="ml-2 md:ml-6 mt-8 flex flex-wrap">
 
 
             {links.map((linkItem) => (<div className="mx-4 lg:mx-6 mb-16" key={linkItem.id}>
@@ -30,7 +30,7 @@ export default function Home({ links, hasMore }) {
               <a target="_blank" href={linkItem.url} rel="noopener noreferrer">
 
                 <Image
-                  className="opacity-70 hover:opacity-100 transition-opacity  duration-300 ease-in-out		 rounded-xl object-cover"
+                    className="opacity-70 hover:opacity-100 transition-opacity  duration-300 ease-in-out 		 rounded-xl object-cover"
                   width={350}
                   height={250}
                   src={linkItem.image || defaultImage}
@@ -44,7 +44,7 @@ export default function Home({ links, hasMore }) {
               </div>
 
 
-              <h2 className="font-roboto my-2 text-2xl text-green-800 font-black">{linkItem.name}</h2>
+              <h2 className="text-center md:text-left font-roboto my-8 text-2xl text-green-800 font-black">{linkItem.name}</h2>
 
 
             </div>))}
